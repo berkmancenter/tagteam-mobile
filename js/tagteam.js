6d97b468c3e0b6eab365edf4b5c7df1495863844
Zhenya <eu.vlasenko@gmail.com>
2012-05-31 11:31:18 +0300

    changed styll eof code in tagteam.js

--- js/tagteam.js
+++ js/tagteam.js
@@ -1,28 +1,26 @@
-$(document).ready(function(){   
-    function getHubs() {
-        $.getJSON('newjson.json', {}, function(json){  
-            var str=''; 
-           
+$.extend({
+    rootPath: function(){
+        return '/';
+    },
+  
+    getHubs:function() {
+        $.getJSON('newjson.json', {}, function(json){             
             for (var key in json.hubs) {
                 var val = json.hubs[key];
-                var output = '<li><a href="hub.html?id='+key+'" data-transition="slidedown"';
-                    output += 'data-rel="dialog" id="'+key+'" class="ui-link-inherit">'
-                +'<h3 class="ui-li-heading">'+val.title+'</h3><p class="ui-li-desc">';
-                            
+                var output = '<li><a href="hub.html?id='+key+'" data-transition="slidedown"'+
+                            'data-rel="dialog" id="'+key+'" class="ui-link-inherit">' +
+                            '<h3 class="ui-li-heading">'+val.title+'</h3><p class="ui-li-desc">';               
                 if (val.description != null) {
                     output+=val.description;
                 }
-                
                 output += '</p></a></li>';
-                
-                $("#hubs").append(output);
-                
+                $("#hubs").append(output);  
             }
             $('#hubs').listview('refresh');
         });     
-    } 
-    
-        getHubs();
-        
-        
+    }
+});
+
+$(document).ready(function(){   
+    $.getHubs(); 
 });
