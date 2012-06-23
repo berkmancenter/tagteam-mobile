dc1052a58146c84e2f75424c86b95c68d63c7641
Zhenya <eu.vlasenko@gmail.com>
2012-06-23 12:22:07 +0300

    introduced API calls

--- js/tagteam.js
+++ js/tagteam.js
@@ -1,6 +1,17 @@
 $.extend({
+    root:function() {
+        return 'http://tagteam.harvard.edu';
+    },
+    
+    links:function(args) {
+        return {
+            'hub':  $.root()+'/hubs.json?callback=?',
+            'items':$.root()+'/hubs/'+$.getLocal('cHubId')+'/items.json?callback=?'
+        }
+    },
+    
     getHubs:function() {
-        $.getJSON('testhubs.json', {}, function(json){
+        $.getJSON($.links().hub, {}, function(json){ 
             $.each(json.hubs, function (key, val) {
                 var desc = '';
                 if (val.description != null) {
@@ -8,32 +19,31 @@ $.extend({
                 }
                 $("#hubs").append(
                     '<li><a href="hub.html" data-transition="slidedown" data-rel="dialog" '+
-                    'id="hub-'+key+'" class="ui-link-inherit">'+'<h3 class="ui-li-heading">'+
+                    'id="hub-'+val.id+'" class="ui-link-inherit">'+'<h3 class="ui-li-heading">'+
                     val.title+'</h3><p class="ui-li-desc">'+desc+'</p></a></li>'); 
             });
             $('#hubs').listview('refresh');
-        }).error(function() {
-            alert("json error");
         });
 	
         $("a[id^='hub']").live('tap',function(e) {
-            $.setLocal('currentHubId', $(this).attr('id').split('-')[1]);
+            $.setLocal('cHubId', $(this).attr('id').split('-')[1]);
         });
     },
     
     getItems:function() {
-        $.getJSON('testitems.json', {}, function(json){  
-            $.each(json.feed_items, function (key, val) {
-                var numbers = val.hub_ids.slice(',');
+        var hub = $.getLocal('cHubId');
+        $.getJSON($.links().items, {}, function(json){  
+            $.each(json.feed_items, function (key, item) {
+                var numbers = item.hub_ids.slice(',');
                 for (var i=0; i<numbers.length; i++) {
-                    if(numbers[i]==localStorage.getItem('currentHubId')) {
+                    if(numbers[i]==hub) {
                         $("#items").append(
-                            '<li><a id="item-'+key+
+                            '<li><a id="item-'+item.id+
                             '" href="./cItem.html" class="ui-link-inherit">'+
-                            '<h3 class="ui-li-heading">'+val.title+'</h3><p class="ui-li-desc">by '+           
-                            val.authors+'</p></a></li>'); 
+                            '<h3 class="ui-li-heading">'+item.title+'</h3><p class="ui-li-desc">by '+           
+                            item.authors+'</p></a></li>'); 
                         $("#item-"+key).live('tap',function(e) {
-                            $.setLocal('currentItemId',key);
+                            $.setLocal('cItemId',key);
                         });
                     }
                 }
@@ -46,7 +56,7 @@ $.extend({
         $.getJSON('hub_feeds.json', {}, function(json){   
             $("#inputs").empty();
             $.each(json.hub_feeds, function(key,val){
-                if (val.hub.id == $.getLocal('currentHubId')) {
+                if (val.hub.id == $.getLocal('cHubId')) {
                     $('#inputs').append('<li><a href="acura.html"><img src="./css/icons/rss-01.png"></img>'+val.title+'<p class="ui-li-desc">'+val.description+'</p></a></li>');
                 }
             });
@@ -55,15 +65,15 @@ $.extend({
     },
     
     getCurrentItem:function() {
-        $.getJSON('testitems.json', {}, function(json){ 
-            var cur = json.feed_items[$.getLocal('currentItemId')];
-            var time = cur.date_published.slice(0,10);
-            $('#title').html(cur.title);
-            $('#published').html(time);
-            $('#updated').html(cur.last_updated)
-            $('#authors').html(cur.authors);
-            $('#url').attr('href',cur.url); 
-            var tags = cur.tags.tags.slice(',');
+        $.getJSON($.links().items, {}, function(json){ 
+            var item = json.feed_items[$.getLocal('cItemId')];
+            $('#itemid').html('Item '+item.id);
+            $('#title').html(item.title);
+            $('#published').html(item.date_published.replace('T',' '));
+            $('#updated').html(item.last_updated.replace('T',' '))
+            $('#authors').html(item.authors);
+            $('#url').attr('href',item.url); 
+            var tags = item.tags.tags.slice(',');
             $('#tags').empty();
             $.each(tags, function(key,val){
                 $('#tags').append('<li><a href="index.html">'+val+'</a></li>');
@@ -99,9 +109,9 @@ $.extend({
     
     debugInfo:function() {
         console.log('page: ' + $('[data-role=page]').attr('id'));
-        console.log('hub id: ' +  $.getLocal('currentHubId'));
+        console.log('hub id: ' +  $.getLocal('cHubId'));
         console.log('---');
-        console.log('cur item:' + $.getLocal('currentItemId'));
+        console.log('cur item:' + $.getLocal('cItemId'));
     }
 });
 
